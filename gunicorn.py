eb5d755653dc3aca23e1e584c0893101a30ca18a
杨岩 <2561952525@qq.com>
2022-08-28 09:26:34 +0800

    gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -6,26 +6,21 @@
 @FileName: gunicorn.py
 """
 
+import os
 import gevent.monkey
-
 gevent.monkey.patch_all()
 
 import multiprocessing
-import os
 
-if not os.path.exists('log'):
-    os.mkdir('log')
-
-debug = True
+# debug = True
 loglevel = 'debug'
-bind = '127.0.0.1:5000'
-pidfile = 'log/gunicorn.pid'
-logfile = 'log/debug.log'
-errorlog = 'log/error.log'
-accesslog = 'log/access.log'
+bind = "0.0.0.0:7001"
+pidfile = "log/gunicorn.pid"
+accesslog = "log/access.log"
+errorlog = "log/debug.log"
+daemon = True
 
 # 启动的进程数
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'gunicorn.workers.ggevent.GeventWorker'
-
+workers = multiprocessing.cpu_count()
+worker_class = 'gevent'
 x_forwarded_for_header = 'X-FORWARDED-FOR'
